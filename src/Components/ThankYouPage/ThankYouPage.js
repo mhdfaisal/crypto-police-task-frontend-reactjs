deb6960afaa20681f9839072baada236a734595e
Mohd <fmohd195@gmail.com>
2019-07-10 14:53:30 +0530

    UserCommentsCard and UserCommentsCard made responsive

--- src/Components/ThankYouPage/ThankYouPage.js
+++ src/Components/ThankYouPage/ThankYouPage.js
@@ -12,13 +12,22 @@ class ThankYouPage extends React.Component {
   state = {
     userComments: [],
     userCommentsSliced: [],
-    showAll: false
+    showAll: false,
+    ratings:0,
+    registrationDate:"",
+    expirationDate:"",
+    phishtankStatus:"",
+    etherscamDb:"",
+    trustworthiness:{
+      color:"",
+      value:""
+    }
   };
 
   componentDidMount() {
     axios
       .post("https://watchdog-api-v1.cryptopolice.com/api/verify", {
-        domain: "https://google.com"
+        domain: this.props.website
       })
       .then(res => {
         console.log(res.data.response);
@@ -30,7 +39,16 @@ class ThankYouPage extends React.Component {
               data.wot.payload.comments.length
             )
           ],
-          userCommentsSliced: [...data.wot.payload.comments.slice(0, 2)]
+          userCommentsSliced: [...data.wot.payload.comments.slice(0, 2)],
+          ratings:data.general_analysis.payload.ratings.watchdog,
+          registrationDate:data.whois.payload.registration.value,
+          expirationDate: data.whois.payload.expiration.value,
+          phishtankStatus:data.phishtank.payload.status.value,
+          etherscamDb: data.etherscam.payload.status.value,
+          trustworthiness:{
+            color:data.wot.payload.trust.color,
+            value:data.wot.payload.trust.value
+          }
         });
       })
       .catch(err => {
@@ -67,14 +85,14 @@ class ThankYouPage extends React.Component {
       <div className={`row ${styles.domainAdditionalInfo}`}>
         <div className="col-md-6 d-flex">
           <div>
-            <p>Domain registration date : 1.12.1992</p>
-            <p>Domain expiration date : 30.11.2019</p>
+            <p>Domain registration date : {this.state.registrationDate}</p>
+            <p>Domain expiration date : {this.state.expirationDate} </p>
           </div>
         </div>
         <div className="col-md-6 d-flex justify-content-end">
           <div>
-            <p>Phishtank status : Nothing Found</p>
-            <p>Etherscam DB : Nothing found</p>
+            <p>Phishtank status : {this.state.phishtankStatus}</p>
+            <p>Etherscam DB : {this.state.etherscamDb}</p>
           </div>
         </div>
       </div>
@@ -84,8 +102,8 @@ class ThankYouPage extends React.Component {
   renderRatingIndicator = () => {
     return (
       <RatingIndicator
-        rating={4.7}
-        starRatedColor="#DAA520"
+        rating={this.state.ratings}
+        starRatedColor={this.state.trustworthiness.color}
         starDimension="40px"
         // changeRating={}
         starSpacing="5px"
@@ -97,10 +115,10 @@ class ThankYouPage extends React.Component {
 
   render() {
     return (
-      <div className="container my-5">
+      <div className={`my-5 ${styles.container}`}>
         <div className="row">
           <div className="col-md-12">
-            <h3 className="text-center mb-4">
+            <h3 className={`text-center mb-4 ${styles.profileHeading}`}>
               Thank you! Your profile is created!
             </h3>
           </div>
@@ -112,13 +130,13 @@ class ThankYouPage extends React.Component {
           </div>
         </div>
         <div className="text-center mt-3">
-          <h5 className="mb-2">Low Risk</h5>
+          <h5 className="mb-2">{this.state.ratings > 2 ? "Low Risk" : "High Risk"}</h5>
           {this.renderRatingIndicator()}
         </div>
         {this.renderDomainAdditionalInfo()}
         <div className="row">
-          <div className="col-md-12 text-center">
-            <h3>Trustworthiness (from api wot/trust): 4.7 / 5.0</h3>
+          <div className={`col-md-12 text-center ${styles.breakHeading}`}>
+            <h4>Trustworthiness: {this.state.trustworthiness.value!=="Nothing found" ? this.state.trustworthiness.value+" / 5.0" : this.state.trustworthiness.value}</h4>
           </div>
         </div>
         <div className="row my-4">{this.renderUserComments()}</div>
